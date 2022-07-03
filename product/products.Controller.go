c1b8217087e793eb860c44f80d82a1290f6ef6cd
amr-zaky <amr.zaky367@yahoo.com>
2022-07-03 17:03:21 +0200

    sdwdw

---
+++ product/products.Controller.go
@@ -0,0 +1,17 @@
+package produc
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
+	"sync"
+)
+
+var productMap = struct {
+	sync.RWMutex
+	m map[int]Product
+    
+}{m: make(map[int]Product)}
